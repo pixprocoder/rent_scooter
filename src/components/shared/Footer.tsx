a87f16bf0c3bcb96fe8dcdc66580732d06f32b1b
samsul kobir <makkobir79@gmail.com>
2023-10-13 04:57:12 +0300

    footer done with responsive

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -9,11 +9,11 @@ import { Button } from "../ui/button";
 
 const Footer = () => {
   return (
-    <footer className=" h-[400px] bg-[#15213C]">
+    <footer className="  lg:h-[400px] bg-[#15213C]">
       <div className="container mx-auto ">
-        <div className="grid grid-cols-4 text-white pt-24">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 text-white pt-24">
           <div>
-            <h3 className="text-xl font-bold  mb-4">Quick Links</h3>
+            <h3 className="text-xl font-bold  my-2 lg:mb-4">Quick Links</h3>
             <ul className="space-y-2 text-sm">
               <li>
                 <Link
@@ -50,7 +50,7 @@ const Footer = () => {
             </ul>
           </div>
           <div>
-            <h3 className="text-xl font-bold  mb-4">Features</h3>
+            <h3 className="text-xl font-bold  my-2 lg:mb-4">Features</h3>
             <ul className="space-y-2 text-sm">
               <li>
                 <Link
@@ -87,7 +87,7 @@ const Footer = () => {
             </ul>
           </div>
           <div>
-            <h3 className="text-xl font-bold  mb-4">Follow US</h3>
+            <h3 className="text-xl font-bold  my-2 lg:mb-4">Follow US</h3>
             <ul className="space-y-2 text-sm">
               <li>Let us be social</li>
               <ul className="flex gap-4">
@@ -115,11 +115,11 @@ const Footer = () => {
             </ul>
           </div>
           <div>
-            <h3 className="text-xl font-bold  mb-4">Newsletter</h3>
+            <h3 className="text-xl font-bold  my-2 lg:mb-4">Newsletter</h3>
             <ul className="space-y-2 text-sm">
               <li>Stay update with our latest</li>
               <li>
-                <div className="flex w-full max-w-sm items-center space-x-2">
+                <div className="flex w-full max-w-sm items-center space-x-2 text-green-700">
                   <Input type="email" placeholder="Email" />
                   <Button type="submit">Subscribe</Button>
                 </div>
